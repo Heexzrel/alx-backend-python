3d5f0b9882719d20e5ff72741f6e46b0a2c67fcb
George Israel <georgeisrael18@outlook.com>
2024-03-29 10:24:12 -0700

    pyvar

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' module.
+'''
+
+
+def floor(a: float) -> int:
+    '''Compute the floor of a float number.
+    '''
+    return int(a)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''module.
+'''
+
+
+def to_str(n: float) -> str:
+    '''Convert a float number to a string.
+    '''
+    return str(n)
